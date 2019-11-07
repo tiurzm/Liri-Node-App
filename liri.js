a347d88c8a5ce524df38207f737f5a95355a84b9
Tiur Mawaddah <tiurzm@gmail.com>
2019-11-07 08:42:29 -0600

    add function for movie and concert search

--- liri.js
+++ liri.js
@@ -10,7 +10,9 @@ var action = process.argv[2];
 var userInput = process.argv;
 
 var concertDetails = "";
+var spotifyDetails = "";
 var movieDetails = "";
+var ratings = [];
 
 switch (action) {
   case "concert-this":
@@ -28,8 +30,61 @@ switch (action) {
   case "do-what-it-says":
     itSays();
     break;
-
 }
+
+// CONCERT
+function concertThis(){
+  for (var i = 3; i < userInput.length; i++) {
+    if (i > 3 && i < userInput.length) {
+      concertDetails = concertDetails + userInput[i];
+    } else {
+      concertDetails += userInput[i];
+    }
+  }
+  console.log(concertDetails)
+  var queryUrl = "https://rest.bandsintown.com/artists/" + concertDetails + "/events?app_id=codingbootcamp"
+  console.log(queryUrl)
+  axios.get(queryUrl).then(
+    function(response) {
+      console.log(response.data);
+      // console.log(response.data.length)
+      // console.log(response.data[0].venue)
+      var resultLength = response.data.length
+      for(var j = 0; j < resultLength; j++) {
+        console.log(`
+          Venue's name: ${response.data[j].venue.name}
+          Location: ${response.data[j].venue.country +", "+ response.data[j].venue.city}
+          Date: ${response.data[j].datetime}
+        `)
+      }
+    }).catch(function(error) {
+      if (error.response) {
+        console.log("---------------Data---------------");
+        console.log(error.response.data);
+        console.log("---------------Status---------------");
+        console.log(error.response.status);
+        console.log("---------------Status---------------");
+        console.log(error.response.headers);
+      } else if (error.request) {
+        console.log(error.request);
+      } else {
+        console.log("Error", error.message);
+      }
+      console.log(error.config);
+    });  
+}
+function spotifyThis(){
+  for (var i = 3; i < userInput.length; i++) {
+    if (i > 3 && i < userInput.length) {
+      spotifyDetails = spotifyDetails + userInput[i];
+    } else {
+      spotifyDetails += userInput[i];
+    }
+  }
+  console.log(spotifyDetails);
+  
+}
+
 
 // MOVIE
 function movieThis(){
@@ -45,7 +100,21 @@ function movieThis(){
   console.log(queryUrl)
   axios.get(queryUrl).then(
     function(response) {
-      console.log(response.data);
+      // console.log(response.data);
+      var resultLength = response.data.Ratings.length
+      for(var j = 0; j < resultLength; j++){
+        ratings.push(response.data.Ratings[j].Value); 
+      }
+      console.log(`
+        Title: ${response.data.Title}
+        Year: ${response.data.Year}
+        IMDB Rating: ${response.data.imdbRating}
+        Rotten Tomatoes Rating: ${ratings}
+        Country: ${response.data.Country}
+        Language: ${response.data.Language}
+        Plot: ${response.data.Plot}
+        Actors: ${response.data.Actors}
+      `)
     })
     .catch(function(error) {
       if (error.response) {
@@ -62,40 +131,4 @@ function movieThis(){
       }
       console.log(error.config);
     });
-
-}
-
-// CONCERT
-function concertThis(){
-  for (var i = 3; i < userInput.length; i++) {
-    if (i > 3 && i < userInput.length) {
-      concertDetails = concertDetails + userInput[i];
-    } else {
-      concertDetails += userInput[i];
-    }
-  }
-  console.log(concertDetails)
-  var queryUrl = "https://rest.bandsintown.com/artists/" + concertDetails + "/events?app_id=codingbootcamp"
-  // "http://www.bandsintown.com/" + concertDetails"
-  console.log(queryUrl)
-  axios.get(queryUrl).then(
-    function(response) {
-      console.log(response.data);
-      console.log(response.data.length)
-      console.log(response.data[0].venue)
-    }).catch(function(error) {
-      if (error.response) {
-        console.log("---------------Data---------------");
-        console.log(error.response.data);
-        console.log("---------------Status---------------");
-        console.log(error.response.status);
-        console.log("---------------Status---------------");
-        console.log(error.response.headers);
-      } else if (error.request) {
-        console.log(error.request);
-      } else {
-        console.log("Error", error.message);
-      }
-      console.log(error.config);
-    });  
 }
